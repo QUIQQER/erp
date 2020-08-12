e5583c5843777accc533ffb2714be371123a5079
Henning Leutz <leutz@pcsg.de>
2020-08-12 15:29:15 +0200

    refactor: quiqqer/invoice#66

---
+++ ajax/products/getProductEdit.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_erp_ajax_products_getProductEdit
+ */
+
+use QUI\ERP\Products\Controls\Products\ProductEdit;
+use QUI\ERP\Products\Handler\Products;
+
+/**
+ *
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_erp_ajax_products_getProductEdit',
+    function ($productId, $user) {
+        $Product = Products::getProduct($productId);
+        $user    = \json_encode($user, true);
+
+        $Control = new ProductEdit([
+            'Product' => $Product
+        ]);
+
+        $css  = ''; //QUI\Control\Manager::getCSS();
+        $html = $Control->create();
+
+        return $css.$html;
+    },
+    ['productId', 'user'],
+    'Permission::checkAdminUser'
+);
---
+++ ajax/products/hasProductCustomFields.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_erp_ajax_products_hasProductCustomFields
+ */
+
+use QUI\ERP\Products\Handler\Products;
+
+/**
+ *
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_erp_ajax_products_hasProductCustomFields',
+    function ($productId) {
+        $Product = Products::getProduct($productId);
+        $fields  = $Product->createUniqueProduct()->getCustomFields();
+
+        return \count($fields);
+    },
+    ['productId'],
+    'Permission::checkAdminUser'
+);
