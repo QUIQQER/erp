5502a4d4d2a489c9f66a5fd351546626f8790927
Henning Leutz <leutz@pcsg.de>
2022-06-30 15:45:56 +0200

    feat: quiqqer/order#134

---
+++ ajax/products/getVariantChildren.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_erp_ajax_products_getVariantChildren
+ */
+
+use QUI\ERP\Products\Handler\Products;
+use QUI\ERP\Products\Product\Types\VariantParent;
+
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_erp_ajax_products_getVariantChildren',
+    function ($productId) {
+        $Product = Products::getProduct($productId);
+
+        if (!($Product instanceof VariantParent)) {
+            return [];
+        }
+
+        return array_map(function ($Variant) {
+            return $Variant->getAttributes();
+        }, $Product->getVariants());
+    },
+    ['productId'],
+    'Permission::checkAdminUser'
+);
