6312cc4340a909958b0a24d4515e960e26657aae
Henning Leutz <leutz@pcsg.de>
2024-04-03 15:20:15 +0200

    feat: centralize erp entity grouping methods

---
+++ ajax/getEntityType.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_erp_ajax_getEntityType
+ */
+
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_erp_ajax_getEntityType',
+    function ($uuid) {
+        $Instance = (new QUI\ERP\Processes)->getEntity($uuid);
+        return get_class($Instance);
+    },
+    ['uuid'],
+    'Permission::checkAdminUser'
+);
