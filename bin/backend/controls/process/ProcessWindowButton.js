b314e1d6a4e12cb7a780517afe207cc9af522008
Henning Leutz <leutz@pcsg.de>
2024-06-20 10:33:09 +0200

    feat: new process window and process window button

---
+++ bin/backend/controls/process/ProcessWindowButton.js
@@ -0,0 +1,54 @@
+/**
+ * Button which opens the window for the process
+ */
+define('package/quiqqer/erp/bin/backend/controls/process/ProcessWindowButton', [
+
+    'qui/QUI',
+    'qui/controls/buttons/Button',
+    'package/quiqqer/erp/bin/backend/controls/process/ProcessWindow',
+    'Locale'
+
+], function(QUI, QUIButton, ProcessWindow, QUILocale) {
+    'use strict';
+
+    const lg = 'quiqqer/erp';
+
+    return new Class({
+
+        Extends: QUIButton,
+        Type: 'package/quiqqer/erp/bin/backend/controls/process/ProcessWindowButton',
+
+        options: {
+            globalProcessId: false,
+            hash: false
+        },
+
+        initialize: function(options) {
+            this.setAttributes({
+                styles: {
+                    'border-left-width': 1,
+                    'border-right-width': 1,
+                    'float': 'right',
+                    width: 40
+                }
+            });
+
+            this.parent(options);
+
+            this.setAttributes({
+                icon: 'fa fa-timeline',
+                alt: QUILocale.get(lg, 'process.button.alt'),
+                title: QUILocale.get(lg, 'process.button.title')
+            });
+
+            this.addEvents({
+                click: () => {
+                    new ProcessWindow({
+                        globalProcessId: this.getAttribute('globalProcessId'),
+                        hash: this.getAttribute('hash')
+                    }).open();
+                }
+            });
+        }
+    });
+});
